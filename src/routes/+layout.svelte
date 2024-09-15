4562cf0728cea822ce28d91edcce4ec2bd4ce8c2
dosc <gamingmaximaal@gmail.com>
2024-09-15 12:43:32 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.css'
+	import { DarkMode } from 'flowbite-svelte'
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+</script>
+
+<Navbar>
+	<NavBrand href="/">
+		<img src="/pancakes-svgrepo-com.svg" class="me-3 h-6 sm:h-9" alt="Pancake Express logo" />
+		<!-- <div class="mr-2 h-10 w-10 overflow-hidden rounded-full">
+			<img src="/truck_logo2.webp" class="scale-125 transform" alt="Pannekoek Express Logo" />
+		</div> -->
+		<!-- <img src="/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>Pancake Express</span
+		>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<DarkMode />
+		<NavHamburger />
+	</div>
+	<NavUl>
+		<NavLi href="/">Over</NavLi>
+		<NavLi href="/pricing">Menu</NavLi>
+		<NavLi href="/about">Volg het spoor</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { Alert } from 'flowbite-svelte'
+
+	import { Banner, Skeleton, ImagePlaceholder } from 'flowbite-svelte'
+	import { SalePercentSolid, ArrowRightOutline } from 'flowbite-svelte-icons'
+
+	import { Carousel } from 'flowbite-svelte'
+
+	import images from '$lib/images.json'
+
+	const duration = 3000
+</script>
+
+<div class="p-8">
+	<Banner id="bottom-banner" position="absolute" bannerType="bottom">
+		<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+			<span class="me-3 inline-flex rounded-full bg-gray-200 p-1 dark:bg-gray-600">
+				<SalePercentSolid class="h-4 w-4 text-gray-500 dark:text-gray-400" />
+				<span class="sr-only">Discount coupon</span>
+			</span>
+			<span>
+				Get 5% commission per sale <a
+					href="https://flowbite.com"
+					class="ms-0 flex items-center text-sm font-medium text-primary-600 hover:underline md:ms-1 md:inline-flex dark:text-primary-500"
+				>
+					Become a partner <ArrowRightOutline class="ms-2 h-3 w-3" />
+				</a>
+			</span>
+		</p>
+	</Banner>
+
+	<Carousel {images} let:Controls>
+		<Controls />
+	</Carousel>
+
+	<Skeleton class="py-4" />
+	<ImagePlaceholder class="py-4" />
+
+	<Alert>
+		<span class="font-medium">Info alert!</span>
+		Change a few things up and try submitting again.
+	</Alert>
+</div>
